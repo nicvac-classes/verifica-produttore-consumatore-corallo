b04f3ef5b3de96c3a1223e358a4327c517178a3e
sigmaclaud <claudiocorallo65@gmail.com>
2026-05-22 08:39:35 +0000

    verifica finita

--- produttore_consumatore_esercizio03B.py
+++ produttore_consumatore_esercizio03B.py
@@ -25,7 +25,20 @@ class ProduttoreThread(threading.Thread):
         super().__init__()
         self.idx = idx
 
-    # DA IMPLEMENTARE (run)
+    def run(self):
+        global metti
+        while(True):
+            vuoto.acquire()
+            mutexP.acquire()
+            i_metti = metti
+            metti=(metti + 1) % DIM_BUFFER
+            mutexP.release()
+            buffer[i_metti] = self.dato
+            print(f"DRN-{self.idx} segnala {genera_drone()} in buffer {i_metti}")
+            self.dato += 1
+            
+            pieno.release()
+
 
 
 class ConsumatoreThread(threading.Thread):
@@ -34,7 +47,22 @@ class ConsumatoreThread(threading.Thread):
         self.idx = idx
 
     # DA IMPLEMENTARE (run)
-
+    def run(self):
+        global togli
+        while(True):
+            if(dato==None):
+                print(f"sentinella di terminazione")
+                continue
+            else:
+                pieno.acquire()
+                mutexC.acquire()
+                i_togli = togli
+                togli = (togli + 1) % DIM_BUFFER
+                mutexC.release()
+                dato = buffer[i_togli]
+                print(f"DRN-{self.idx} autorizza atterraggio {genera_drone()}")
+                
+                vuoto.release()
 
 def main():
     global metti
@@ -43,20 +71,27 @@ def main():
     consumatori = [ConsumatoreThread(i + 1) for i in range(N_CONSUMATORI)]
 
     # DA IMPLEMENTARE: start dei thread produttori e consumatori
-
+    for c in consumatori:
+        c.start()
+    for p in produttori:
+        p.start()
     # DA IMPLEMENTARE: join di tutti i produttori
-
+    for p in produttori:
+        p.join()
     print("Tutti i sensori hanno terminato. Chiusura piste...")
 
     # Invia una sentinella None per ogni pista attiva.
     for _ in range(N_CONSUMATORI):
         # DA IMPLEMENTARE: inserire None nel buffer
+        vuoto.acquire()
+        buffer[metti] = None
+        metti = (metti + 1) % DIM_BUFFER
+        pieno.release()  
         pass
-
     # DA IMPLEMENTARE: join di tutti i consumatori
-
+    for c in consumatori:
+            c.join
     print("Torre operativa chiusa.")
-
-
+   
 if __name__ == "__main__":
     main()
